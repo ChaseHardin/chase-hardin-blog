7aa6c0b451329d60bb77439bf38a288612539259
Chase Hardin <chase.hardin@scribbletechnology.com>
2019-02-23 09:30:01 -0600

    Configure Jest testing

---
+++ src/components/hello-world.component.spec.js
@@ -0,0 +1,14 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import HelloWorld from "./hello-world.component"
+
+describe("Hello World", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<HelloWorld />)
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+})
