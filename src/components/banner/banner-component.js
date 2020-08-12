7a3b72aa1b794cd516a89919340f49264e26a68c
Chase Hardin <chase.hardin@scribbletechnology.com>
2020-08-11 21:01:55 -0500

    Remove banner

--- src/components/banner/banner-component.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import './banner.css'
-
-export default function Banner() {
-  return <section className='working-on-computer'/>
-}
--- src/components/layout/layout-component.js
+++ src/components/layout/layout-component.js
@@ -1,11 +1,9 @@
 import React from "react"
 import Menu from "../menu/menu-component"
-import Banner from "../banner/banner-component"
 
 export default function Layout({ children }) {
   return (
     <React.Fragment>
-      <Banner/>
       <Menu/>
       <div style={{ margin: `3rem auto`, maxWidth: 750, padding: `0 1rem` }}>
         {children}
