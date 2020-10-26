fe8863e799ad6b521dbce248c99113b8ff9b5b16
Chase Hard <dchasehardin@gmail.com>
2020-10-25 20:59:28 -0500

    Cleanup menu

--- src/components/layout/layout-component.js
+++ src/components/layout/layout-component.js
@@ -1,50 +1,16 @@
 import React from "react"
 import Banner from "../banner/banner-component"
 import 'fontsource-roboto';
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { Link } from "gatsby"
-import Grid from '@material-ui/core/Grid';
-
-const renderAppBar = () => {
-  return (
-    <AppBar position="fixed" style={{ backgroundColor: '#242526' }}>
-      <Toolbar style={{ display: 'flex' }}>
-        <div className='profile-menu-picture-container'>
-          <img src={require("../../images/profile_pic.png")} className='profile-menu-picture' alt={""} />
-        </div>
-        <Link to={'/'} style={{ marginRight: '20px' }}>
-          <Typography variant="h6" noWrap style={{ color: 'white' }}>
-            Chase Hardin
-          </Typography>
-        </Link>
-        <Grid
-          container
-          direction="row"
-          justify="flex-end"
-          alignItems="center"
-        >
-          <Link to={'/about/'} style={{ color: 'white' }}>
-            <Typography variant="h6">
-              About
-          </Typography>
-          </Link>
-        </Grid>
-      </Toolbar>
-    </AppBar>
-  )
-}
+import {Menu} from '../menu';
 
 export default function Layout({ children }) {
   return (
-    <React.Fragment>
-      {renderAppBar()}
-      <Toolbar />
+    <>
+      <Menu />
       <Banner />
       <div style={{ margin: `3rem auto`, maxWidth: 750, padding: `0 1rem` }}>
         {children}
       </div>
-    </React.Fragment>
+    </>
   )
 }
